32f527c5942594ae722963bf74ffc6dd759e81a2
alanzeng1213 <alanzeng1213@gmail.com>
2018-12-21 12:03:29 +0800

    Create my new branch test

--- src/com/zeng/Happy.java
+++ src/com/zeng/Happy.java
@@ -1,13 +1,10 @@
 package com.zeng;
 
 public class Happy {
-<<<<<<< HEAD
    int age =12;
    String name ="1212";
-=======
  
 	public static void main(String[] args) {
 		System.out.println();
 	}
->>>>>>> branch 'master' of https://github.com/alanzeng1213/mytestGit.git
 }
